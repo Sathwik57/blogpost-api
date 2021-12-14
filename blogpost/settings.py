f3dc42bbaac0337b6f7db4b1d6adc307beb269d3
Sathwik <sathwikbhimavarapu@gmail.com>
2021-12-14 12:08:24 +0530

    added host

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY' , get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['64.227.184.15',]
+ALLOWED_HOSTS = ['64.227.184.15', ]
 
 
 # Application definition
